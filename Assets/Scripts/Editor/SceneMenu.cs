38522b9f1bd42c1eedc21b1fc29d6db210bdf437
MichVawer <michelle.vawer@icloud.com>
2022-03-30 17:48:04 -0400

    SceneUtils created scene menu for editor drop down scene menu change

--- Assets/Scripts/Editor/SceneMenu.cs
+++ Assets/Scripts/Editor/SceneMenu.cs
@@ -3,29 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
+using UnityEditor;
 
 
 public static class SceneMenu
 {
+    [MenuItem("Scenes/SampleScene")]
     static void OpenSampleScene()
     {
-
+        OpenScene(SceneUtils.Names.SampleScene);
     }
 
+    [MenuItem("Scenes/Scene2")]
     static void OpenScene2()
     {
-
+        OpenScene(SceneUtils.Names.Scene2);
     }
 
+    [MenuItem("Scenes/SampleScene3")]
     static void OpenScene3()
     {
-
+        OpenScene(SceneUtils.Names.Scene3);
     }
 
 
     static void OpenScene(string name)
     {
-
+        Scene PersistentScene = EditorSceneManager.OpenScene("Assets/Scenes/" + SceneUtils.Names.XRPersistent + ".unity", OpenSceneMode.Single);
+        Scene currentScene = EditorSceneManager.OpenScene("Assets/Scenes/" + name + ".unity", OpenSceneMode.Additive);
     }
 
 
