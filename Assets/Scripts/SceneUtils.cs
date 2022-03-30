551bbe1a884377004f6b39f5d1aa7df88355922c
MichVawer <michelle.vawer@icloud.com>
2022-03-30 18:02:24 -0400

    Added function to align XRRig with XROrigin

--- Assets/Scripts/SceneUtils.cs
+++ Assets/Scripts/SceneUtils.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public static class SceneUtils
 {
@@ -11,9 +12,29 @@ public static class SceneUtils
         public static readonly string SampleScene = "SampleScene";
         public static readonly string Scene2 = "Scene2";
         public static readonly string Scene3 = "Scene3";
+    }
 
+    public static void AlignXRRig(Scene persistentScene,  Scene currentScene)
+    {
+        GameObject[] currentObjects = currentScene.GetRootGameObjects();
+        GameObject[] persistentObjects = persistentScene.GetRootGameObjects();
+
+        foreach (var origin in currentObjects)
+            if(origin.CompareTag("XRRigOrigin"))
+            {
+                 foreach(var rig in persistentObjects)
+                 {
+                    if(rig.CompareTag("XRRig"))
+                    {
+                        rig.transform.position = origin.transform.position;
+                        rig.transform.rotation = origin.transform.rotation;
+                        return;
+                    }
+                 }
+            }
 
     }
+
 
 }
 
