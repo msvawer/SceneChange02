29624a28754e08b5ba544f9a26436e4036f9f1ac
MichVawer <michelle.vawer@icloud.com>
2022-03-30 17:26:50 -0400

    Created SceneUtils script and Editor folder with SceneMenu script

---
+++ Assets/Scripts/Editor/SceneMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+
+
+public static class SceneMenu
+{
+    static void OpenSampleScene()
+    {
+
+    }
+
+    static void OpenScene2()
+    {
+
+    }
+
+    static void OpenScene3()
+    {
+
+    }
+
+
+    static void OpenScene(string name)
+    {
+
+    }
+
+
+}
+
---
+++ Assets/Scripts/SceneUtils.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SceneUtils
+{
+    public static class Names
+    {
+        //*The scene names in the quotes need to be the exact names of the actual scenes
+        public static readonly string XRPersistent = "XRPersistent";
+        public static readonly string SampleScene = "SampleScene";
+        public static readonly string Scene2 = "Scene2";
+        public static readonly string Scene3 = "Scene3";
+
+
+    }
+
+}
+
+
+
